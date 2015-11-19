d9a4f1fb2600f395b021fda8dfb124f4800c9d4a
DonFreed <donfreed12@gmail.com>
2015-11-19 14:33:00 -0500

    Initial commit.
    Works to merge two bam files and copy a header from either one of the bam files or another file.

---
+++ Makefile
@@ -0,0 +1,23 @@
+CC       = gcc
+CPPFLAGS = 
+CFLAGS   = -g -Wall #-O2
+LDFLAGS  =
+LIBS     =
+
+INCLUDES = -I . -I $(HTSDIR)
+
+HTSDIR = /usr/local/lib
+HTSLIB = $(HTSDIR)/libhts.a
+
+all: submixbam
+
+.SUFFIXES: .c .o
+
+%.o : %.c
+	$(CC) -c $(CFLAGS) $(INCLUDES) $< -o $@
+
+submixbam: submixbam.o bedidx.o $(HTSLIB)
+	$(CC) -pthread $(LDFLAGS) -o $@ submixbam.o bedidx.o $(HTSLIB) $(LIBS) -lz
+
+submixbam.o: submixbam.c
+bedidx.o: bedidx.c $(HTSDIR)/htslib/ksort.h $(HTSDIR)/htslib/kseq.h $(HTSDIR)/htslib/khash.h
---
+++ submixbam.c
@@ -0,0 +1,238 @@
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include "htslib/hts.h"
+#include "htslib/sam.h"
+
+#define IS_EMPTY UINT64_MAX
+
+// bedidx declarations //
+void *bed_read(const char *fn);
+int bed_overlap(const void *_h, const char *chr, int beg, int end);
+void bed_destroy(void *_h);
+
+
+void usage(FILE *fp)
+{
+    fprintf(fp,
+"\n"
+"submixbam\n"
+"  Randomly subset from and merge sequence reads from two bam files over regions\n"
+"\n"
+"Usage: subsetbam [options] [-h inh.sam] <in1.bam> <in2.bam> <in.bed>\n"
+"\n"
+"Options:\n"
+"  -h FILE      copy the header in FILE to the output [in1.bam]\n"
+"  -o FILE      write output to FILE [stdout]\n"
+"  -m FLOAT     minimum faction of reads from <in1.bam> [0.0]\n"
+"  -a FLOAT     maximum faction of reads from <in1.bam [1.0]\n"
+"  -s VALUE     use VALUE to initalize the random seed [time]\n"
+"  -d FLOAT     downsample from <in1.bam> by this fraction [1.0]\n"
+"  -e FLOAT     downsample from <in2.bam> by this fraction [1.0]\n"
+"  -c INT       compression level for the output file [0]\n");
+}
+
+int main(int argc, char *argv[])
+{
+    int c, compression = 0;
+    char *fn_in1 = 0, *fn_in2 = 0, *fn_bed, *fn_out = 0, *fn_hdr = 0, mode[16];
+    long random_seed = (long)time(NULL);
+    float min_frac = 0.0, max_frac = 1.0, down1 = 1.0, down2 = 1.0;
+    void *bed = 0;
+    samFile *fp_out = 0, *fp_in1 = 0, *fp_in2 = 0;
+    bam_hdr_t *hdr = 0, *hout = 0;
+    hts_idx_t *idx = 0;
+    hts_itr_t *iter1 = 0, *iter2 = 0;
+    bam1_t *b1 = 0, *b2 = 0;
+    uint64_t pos1 = 0, pos2 = 0;
+
+    while ((c = getopt(argc, argv, "h:o:m:a:s:d:e:c:")) >= 0) {
+        if (c == 'h') fn_hdr = strdup(optarg);
+        else if (c == 'o') fn_out = strdup(optarg);
+        else if (c == 'm') min_frac = atof(optarg);
+        else if (c == 'a') max_frac = atof(optarg);
+        else if (c == 's') random_seed = atol(optarg);
+        else if (c == 'd') down1 = atof(optarg);
+        else if (c == 'e') down2 = atof(optarg);
+        else if (c == 'c') compression = atoi(optarg);
+    }
+    // User errors //
+    if (argc - optind< 3 || argc - optind > 3) {
+        usage(stderr);
+        return 1;
+    }
+    if (min_frac < 0.0 || max_frac < 0.0) { 
+        fprintf(stderr, "Warning: minimum fraction of reads can not be set below 0\n");
+        min_frac = min_frac < 0.0 ? 0.0 : min_frac;
+        max_frac = max_frac < 0.0 ? 0.0 : max_frac;
+    }
+    if (min_frac > 1.0 || max_frac > 1.0) {
+        fprintf(stderr, "Warning: minimum fraction of reads can not be set above 1.0\n");
+        min_frac = min_frac > 1.0 ? 1.0 : min_frac;
+        min_frac = max_frac > 1.0 ? 1.0 : max_frac;
+    }
+    if (min_frac >= max_frac) {
+        fprintf(stderr, "Error: the minimum fraction of reads must be below the maximum fraction of reads\n");
+        return 1;
+    }
+    if (down1 > 1.0 || down1 < 0.0) {
+        fprintf(stderr, "Error: '-d' must be between 0.0 and 1.0\n");
+        return 1;
+    }
+    if (down2 > 1.0 || down2 < 0.0) {
+        fprintf(stderr, "Error: '-e' must be between 0.0 and 1.0\n");
+        return 1;
+    }
+    if (compression < 0)
+        compression = 0;
+    else if (compression > 9)
+        compression = 9;
+
+    fprintf(stderr, "1\n");
+    srand48(random_seed);
+    fn_in1 = strdup(argv[optind]);
+    fn_in2 = strdup(argv[optind + 1]);
+    fn_bed = strdup(argv[optind + 2]);
+    if (!(bed = bed_read(fn_bed))) {
+        fprintf(stderr, "Error: error reading bed file\n");
+        return 1;
+    }
+    // The hard way //
+    mode[0] = 'w'; mode[1] = 'b'; mode[2] = compression + '0'; mode[3] = 0;
+    fprintf(stderr, "%s\n", mode);
+    if (!(fp_out = sam_open(fn_out ? fn_out : "-", mode))) {
+        fprintf(stderr, "Error: could not open output file %s\n", fn_out);
+        return 1;
+    }
+    if (!(fp_in1 = sam_open(fn_in1, "rb"))) {
+        fprintf(stderr, "Error: could not open input file %s\n", fn_in1);
+        return 1;
+    }
+    if (!(fp_in2 = sam_open(fn_in2, "rb"))) {
+        fprintf(stderr, "Error: could not open input file %s\n", fn_in2);
+        return 1;
+    }
+    fprintf(stderr, "2\n");
+
+    // Read the header and write to the output file //
+    hdr = bam_hdr_init();
+    hout = bam_hdr_init();
+    if (fn_hdr) {
+        htsFile *fp_hdr = hts_open(fn_hdr, "r");
+        if (!fp_hdr) {
+            fprintf(stderr, "Error: could not read header file\n");
+            return 1;
+        }
+        hout = sam_hdr_read(fp_hdr);
+        if (sam_hdr_write(fp_out, hout)) {
+            fprintf(stderr, "Error: could not write header\n");
+            return 1;
+        }
+        // Read other headers
+        hdr = sam_hdr_read(fp_in1);
+        bam_hdr_destroy(hdr);
+        hdr = sam_hdr_read(fp_in2);
+        bam_hdr_destroy(hdr);
+    } else {
+        hout = sam_hdr_read(fp_in1);
+        if (sam_hdr_write(fp_out, hout)) {
+            fprintf(stderr, "Error: could not wirte header\n");
+            return 1;
+        }
+        // Read other header
+        hdr = sam_hdr_read(fp_in2);
+        bam_hdr_destroy(hdr);
+    }
+    fprintf(stderr, "3\n");
+    fprintf(stderr, "hout has %" PRId32 " targets\n", hout->n_targets);
+
+    // Initalize iterators //
+    if (!(idx = sam_index_load(fp_in1, fn_in1))) {
+        fprintf(stderr, "Could not load index of file %s\n", fn_in1);
+        return 1;
+    }
+    if (!(iter1 = sam_itr_queryi(idx, HTS_IDX_START, 0, 0))) {
+        fprintf(stderr, "Could not load iterator for file %s\n", fn_in1);
+        return 1;
+    }
+    hts_idx_destroy(idx);
+    if (!(idx = sam_index_load(fp_in2, fn_in2))) {
+        fprintf(stderr, "Could not load index of file %s\n", fn_in2);
+        return 1;
+    }
+    if (!(iter2 = sam_itr_queryi(idx, HTS_IDX_START, 0, 0))) {
+        fprintf(stderr, "Could not load iterator for file %s\n", fn_in2);
+        return 1;
+    }
+
+    fprintf(stderr, "iter1 has tid %d\n", iter1->tid);
+    fprintf(stderr, "iter2 has tid %d\n", iter2->tid);
+
+    hts_idx_destroy(idx);
+    fprintf(stderr, "4\n");
+    b1 = bam_init1();
+    b2 = bam_init1();
+    if ((c = sam_itr_next(fp_in1, iter1, b1)) >= 0) {
+        pos1 = ((uint64_t)b1->core.tid<<32) | (uint32_t)((uint32_t)b1->core.pos+1)<<1 | bam_is_rev(b1);
+        fprintf(stderr, "Read bam\n");
+    } else {
+        fprintf(stderr, "Did not read bam\n");
+        pos1 = IS_EMPTY;
+    }
+    if ((c = sam_itr_next(fp_in2, iter2, b2)) >= 0) {
+        pos2 = ((uint64_t)b2->core.tid<<32) | (uint32_t)((uint32_t)b2->core.pos+1)<<1 | bam_is_rev(b2);
+        fprintf(stderr, "Read bam\n");
+    } else {
+        pos2 = IS_EMPTY;
+        fprintf(stderr, "Did not read bam\n");
+    }
+    // Merge data //
+    fprintf(stderr, "5\n");
+    while (pos1 != IS_EMPTY || pos2 != IS_EMPTY) {
+        if (pos2 < pos1) {
+            sam_write1(fp_out, hout, b2);
+            if (sam_itr_next(fp_in2, iter2, b2) >= 0) {
+                pos2 = ((uint64_t)b2->core.tid<<32) | (uint32_t)((uint32_t)b2->core.pos+1)<<1 | bam_is_rev(b2);
+            } else {
+                pos2 = IS_EMPTY;
+            }
+        } else {
+            sam_write1(fp_out, hout, b1);
+            if (sam_itr_next(fp_in1, iter1, b1) >= 0) {
+                pos1 = ((uint64_t)b1->core.tid<<32) | (uint32_t)((uint32_t)b1->core.pos+1)<<1 | bam_is_rev(b1);
+            } else {
+                pos1 = IS_EMPTY;
+            }
+        }
+    }
+
+    printf("min_frac is %f\n", min_frac);
+    printf("max_frac is %f\n", max_frac);
+    printf("down1 is %f\n", down1);
+    printf("down2 is %f\n", down2);
+    printf("seed is %ld\n", random_seed);
+    printf("fn1 is %s\n", fn_in1);
+    printf("fn2 is %s\n", fn_in2);
+    printf("fn_bed is %s\n", fn_bed);
+    printf("fn_hdr is %s\n", fn_hdr? fn_hdr : "not specified");
+    printf("fn_out is %s\n", fn_out? fn_out : "not specified");
+
+    free(fn_out);
+    free(fn_in1);
+    free(fn_in2);
+    free(fn_bed);
+    bed_destroy(bed);
+    hts_close(fp_out);
+    hts_close(fp_in1);
+    hts_close(fp_in2);
+    bam_hdr_destroy(hout);
+    hts_itr_destroy(iter1);
+    hts_itr_destroy(iter2);
+    bam_destroy1(b1);
+    bam_destroy1(b2);
+    return 0;
+}
